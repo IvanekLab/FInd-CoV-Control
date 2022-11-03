3ee0332355b6d410215c707a61d8eccfc42ae5f4
Chris Henry <cjh57@cornell.edu>
2022-11-02 22:58:00 -0400

    Updated unique_id values.

--- iFoodDS-wrapper-v2.2.0.R
+++ iFoodDS-wrapper-v2.2.0.R
@@ -361,7 +361,7 @@ do.call(full_run,
           additional_facility_parameters,
           list(sensitivity_variable=NULL,
                sensitivity_multiplier=1,
-               unique_id = 'facility-pass-3')))
+               unique_id = 'facility-pass-4')))
 for(sensitivity_variable in names(kConstants)) {
     for(sensitivity_multiplier in c(0.5, 1.5)) {
         kConstants = kConstants_
@@ -371,7 +371,7 @@ for(sensitivity_variable in names(kConstants)) {
                   additional_facility_parameters,
                   list(sensitivity_variable=sensitivity_variable,
                        sensitivity_multiplier=sensitivity_multiplier,
-                       unique_id = paste0('facility-pass-3-', sensitivity_variable, '-', sensitivity_multiplier))))
+                       unique_id = paste0('facility-pass-4-', sensitivity_variable, '-', sensitivity_multiplier))))
     }
 }
 stop('Made it through.')
@@ -382,7 +382,7 @@ do.call(full_run,
           additional_facility_parameters,
           list(sensitivity_variable=NULL,
                sensitivity_multiplier=1,
-               unique_id = 'facility-pass-3')))
+               unique_id = 'facility-pass-4')))
 for(sensitivity_variable in names(kConstants)) {
     for(sensitivity_multiplier in c(0.5, 1.5)) {
         kConstants = kConstants_
@@ -391,7 +391,7 @@ for(sensitivity_variable in names(kConstants)) {
                   additional_facility_parameters,
                   list(sensitivity_variable=sensitivity_variable,
                        sensitivity_multiplier=sensitivity_multiplier,
-                       unique_id = paste0('facility-pass-3-', sensitivity_variable, '-', sensitivity_multiplier))))
+                       unique_id = paste0('facility-pass-4-', sensitivity_variable, '-', sensitivity_multiplier))))
     }
 }
 kConstants = kConstants_
