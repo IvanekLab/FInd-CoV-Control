746d49e30045884bae5fb433b0bf95a2f914e244
Chris Henry <cjh57@cornell.edu>
2023-02-08 03:14:55 -0500

    Got this working, just need to add the specific pass 2-4 plots.

--- scenario-panel-plot.R
+++ scenario-panel-plot.R
@@ -1,53 +1,119 @@
-image11 = readPNG('scenario-analysis-take-2/farm-shared-random-startbaseline_Total-Symptomatic-Infections-violin_2.2.0.png')
-image12 = readPNG('scenario-analysis-take-2/farm-shared-random-startbaseline_Average-Unavailable-production-violin_2.2.0.png')
-image13 = readPNG('scenario-analysis-take-2/farm-shared-random-startbaseline_Total-Cost-violin_2.2.0.png')
+#Annotation scheme:
+#   capital for setting, lower-case for value
+#   (old idea, ignore) lower-case for (tentative) "default," capital for altered
+#Setting
+#   r: farm
+#   c: facility
+#Housing:
+#   s: shared
+#   i: individual
+#Vaccination:
+#   d: default
+#   0: no vaccination
+#   1: max vaccination (not yet present)
+#Recovery:
+#   d: default
+#   0: no recovered
+#   1: max recovered (not yet present)
 
-image21 = readPNG('scenario-analysis-take-2/facility-shared-random-start_Total-Symptomatic-Infections-violin_2.2.0.png')
-image22 = readPNG('scenario-analysis-take-2/facility-shared-random-start_Average-Unavailable-production-violin_2.2.0.png')
-image23 = readPNG('scenario-analysis-take-2/facility-shared-random-start_Total-Cost-violin_2.2.0.png')
+library(png)
+library(ggplot2)
+library(grid)
+library(cowplot)
 
-image31 = readPNG('scenario-analysis-take-2/facility-individual-random-startbaseline_Total-Symptomatic-Infections-violin_2.2.0.png')
-image32 = readPNG('scenario-analysis-take-2/facility-individual-random-startbaseline_Average-Unavailable-production-violin_2.2.0.png')
-image33 = readPNG('scenario-analysis-take-2/facility-individual-random-startbaseline_Total-Cost-violin_2.2.0.png')
+images = list()
+plots = list()
 
-image41 = readPNG('scenario-analysis-take-2/farm-individual-random-start_Total-Symptomatic-Infections-violin_2.2.0.png')
-image42 = readPNG('scenario-analysis-take-2/farm-individual-random-start_Average-Unavailable-production-violin_2.2.0.png')
-image43 = readPNG('scenario-analysis-take-2/farm-individual-random-start_Total-Cost-violin_2.2.0.png')
+for(setting in c('farm', 'facility')) {#, 'facility')) {
+        #key = paste0('S', setting[3])
+        #filename = paste0('scenario-analysis-take-2/',
+    for(housing in c('shared', 'individual')) {
+        for(vaccination in c('', 'no-vax')) {
+            for(recovery in c('', 'no-recovered')) {
+                vax_key = ifelse(vaccination == '',
+                    'd',
+                    ifelse(vaccination == 'no-vax',
+                        '0',
+                        '1'
+                    )
+                )
+                recovery_key = ifelse(recovery == '',
+                    'd',
+                    ifelse(recovery == 'no-recovered',
+                        '0',
+                        '1'
+                    )
+                )
+                #merged_vax_recovered_filename = ifelse(vax_key == '0' && recovery_key == '0'
+                if(vax_key == '0' && recovery_key == '0') {
+                    merged_fragment = '-start-of-epidemic'
+                } else if(vax_key == 'd' && recovery_key == 'd') {
+                    merged_fragment = ''
+                } else {
+                    merged_fragment = paste0('-', vaccination, recovery)
+                }
+                key_base = paste0(
+                    'S:', substr(setting, 3, 3), ', ',
+                    'H:', substr(housing, 1, 1), ', ',
+                    'V:', vax_key, ', ',
+                    'R:', recovery_key
+                )
+                is_baseline = (
+                    (
+                        (setting == 'farm' && housing == 'shared') ||
+                        (setting == 'facility' && housing == 'individual')
+                    ) &&
+                    merged_fragment == ''
+                )
+                filename_base = paste0(
+                    'scenario-analysis-take-2/',
+                    setting, '-',
+                    housing, '-random-start',
+                    merged_fragment,
+                    ifelse(is_baseline, 'baseline_', '_')
+                )
+                cat(key_base, ':', filename_base, '\n')
+                if(setting == 'farm' || (merged_fragment == '')) {
+                    for(outcome in c(
+                        'Total-Symptomatic-Infections',
+                        'Average-Unavailable-production',
+                        'Total-Cost'
+                    )) {
+                        filename = paste0(
+                            filename_base,
+                            outcome,
+                            '-violin_2.2.0.png'
+                        )
+                        key = paste0(key_base, ': ', outcome)
+                        images[[key]] = readPNG(filename)
+                        plots[[key]] = ggplot() + annotation_custom(
+                            grid::rasterGrob(
+                                images[[key]],
+                                width = unit(1, 'npc'),
+                                height = unit(1, 'npc')
+                            )
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
 
-
-plot_11 = ggplot() + annotation_custom(grid::rasterGrob(image11, width = unit(1, 'npc'), height = unit(1, 'npc')))
-plot_12 = ggplot() + annotation_custom(grid::rasterGrob(image12, width = unit(1, 'npc'), height = unit(1, 'npc')))
-plot_13 = ggplot() + annotation_custom(grid::rasterGrob(image13, width = unit(1, 'npc'), height = unit(1, 'npc')))
-
-plot_21 = ggplot() + annotation_custom(grid::rasterGrob(image21, width = unit(1, 'npc'), height = unit(1, 'npc')))
-plot_22 = ggplot() + annotation_custom(grid::rasterGrob(image22, width = unit(1, 'npc'), height = unit(1, 'npc')))
-plot_23 = ggplot() + annotation_custom(grid::rasterGrob(image23, width = unit(1, 'npc'), height = unit(1, 'npc')))
-
-plot_31 = ggplot() + annotation_custom(grid::rasterGrob(image31, width = unit(1, 'npc'), height = unit(1, 'npc')))
-plot_32 = ggplot() + annotation_custom(grid::rasterGrob(image32, width = unit(1, 'npc'), height = unit(1, 'npc')))
-plot_33 = ggplot() + annotation_custom(grid::rasterGrob(image33, width = unit(1, 'npc'), height = unit(1, 'npc')))
-
-plot_41 = ggplot() + annotation_custom(grid::rasterGrob(image41, width = unit(1, 'npc'), height = unit(1, 'npc')))
-plot_42 = ggplot() + annotation_custom(grid::rasterGrob(image42, width = unit(1, 'npc'), height = unit(1, 'npc')))
-plot_43 = ggplot() + annotation_custom(grid::rasterGrob(image43, width = unit(1, 'npc'), height = unit(1, 'npc')))
-
-#plot_grid(plot_11, plot_12, plot_21, plot_22)
-
-#plot_11 = ggplot() + annotation_custom(grid::rasterGrob(image11, width = unit(1, 'npc'), height = unit(1, 'npc')))
-#plot_12 = ggplot() + annotation_custom(grid::rasterGrob(image12, width = unit(1, 'npc'), height = unit(1, 'npc')))
-#plot_13 = ggplot() + annotation_custom(grid::rasterGrob(image13, width = unit(1, 'npc'), height = unit(1, 'npc')))
-
-
-#plot_21 = ggplot() + annotation_custom(grid::rasterGrob(image21, width = unit(1, 'npc'), height = unit(1, 'npc')))
-#plot_22 = ggplot() + annotation_custom(grid::rasterGrob(image22, width = unit(1, 'npc'), height = unit(1, 'npc')))
-
-png('scenario-panel-plots-version-2.png', height = 4*1000, width = 3 * 1300)
-plot_grid(plot_11, plot_12, plot_13,
-          plot_21, plot_22, plot_23,
-          plot_31, plot_32, plot_33,
-          plot_41, plot_42, plot_43,
-          nrow = 4#,
-          #byrow = FALSE
+png('scenario-panel-plots-pass-1.png', height = 4*1000, width = 3 * 1300)
+keys = c(
+    'S:r, H:s, V:d, R:d: Total-Symptomatic-Infections',
+    'S:r, H:s, V:d, R:d: Average-Unavailable-production',
+    'S:r, H:s, V:d, R:d: Total-Cost',
+    'S:c, H:s, V:d, R:d: Total-Symptomatic-Infections',
+    'S:c, H:s, V:d, R:d: Average-Unavailable-production',
+    'S:c, H:s, V:d, R:d: Total-Cost',
+    'S:c, H:i, V:d, R:d: Total-Symptomatic-Infections',
+    'S:c, H:i, V:d, R:d: Average-Unavailable-production',
+    'S:c, H:i, V:d, R:d: Total-Cost',
+    'S:r, H:i, V:d, R:d: Total-Symptomatic-Infections',
+    'S:r, H:i, V:d, R:d: Average-Unavailable-production',
+    'S:r, H:i, V:d, R:d: Total-Cost'    
 )
+print(plot_grid(plotlist = plots[keys], nrow = 4))
 dev.off()
-
