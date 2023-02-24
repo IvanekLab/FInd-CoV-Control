89fb467622e08ff7fbfe72223f3834ff8eac3b94
Chris Henry <cjh57@cornell.edu>
2023-02-24 10:40:55 -0500

    Measuring number of secondary transmissions from the index case, using that as a predictor.

--- pairwise-scatter-plots.R
+++ pairwise-scatter-plots.R
@@ -1,5 +1,7 @@
 d1 = apply(readRDS('sat2-with-iii/farm-sharedbaseline_community-0,work_R0-6,dormitory_R0-2,E0-1,initial_recovered-71,initial_V2-73,n_sims-1000index_i-1_full-output.rds')[,'new_symptomatic_infections',], 2, sum)
 
+d1iii = apply(readRDS('sat2-with-iii/farm-sharedbaseline_community-0,work_R0-6,dormitory_R0-2,E0-1,initial_recovered-71,initial_V2-73,n_sims-1000index_i-1_full-output.rds')[,'iii',], 2, sum)
+
 
 filenames = c(
     'sat2-with-iii/farm-sharedbaseline_community-0,work_R0-6,dormitory_R0-2,E0-1,T.test-38,initial_recovered-71,initial_V2-73,n_sims-1000index_i-2_full-output.rds',
@@ -19,6 +21,9 @@ filenames = c(
 d = lapply(filenames,  function(s) readRDS(s))
 d_ = lapply(d, function(x) x[,'new_symptomatic_infections',])
 d__ = lapply(d_, function(x) apply(x, 2, sum))
+
+d_iii = lapply(d, function(x) x[,'iii',])
+d__iii = lapply(d_iii, function(x) apply(x, 2, sum))
 
 #library(png)
 #library(ggplot2)
@@ -41,17 +46,23 @@ row.names<-c(
         'Vax + Boosting, p = 0.02/day'
     )[-1]
 
-png('pairwise-scatterplots-with-iii.png', height = 3 * 1000, width = 4 * 1000)
+png('pairwise-scatterplots-with-iii-vs-iii.png', height = 3 * 1000, width = 4 * 1000)
 l = 0:5 * 2 + 1
 layout(matrix(c(l, l + 1, l + 12, l+ 13), byrow = T, ncol = 6))
 #for(x in d__) {
 for(i in 1:12) {
     x = d__[[i]]
+    xiii = d__iii[[i]]
     par(mar = c(12, 12, 8, 8), mgp = c(8, 2, 0))
-    plot(d1, x, main = row.names[i], xlim = c(0, 71), ylim = c(0, 73), lwd = 2, cex.axis = 4, cex.names=4, cex.lab=4, cex.main=4, xlab = 'y_{j0m}', ylab = 'y_{jkm}')
-    points(c(0, 73), c(0, 73), type = 'l', col = 'blue', lwd = 2)
-    points(c(0, 73), c(0, 0), type = 'l', col = 'green', lwd = 2)
-    plot(d1, x - d1, xlim = c(0, 71), ylim = c(-71, 21), lwd = 2, cex.axis = 4, cex.names=4, cex.lab=4, cex.main=4, xlab = 'y_{j0m}', ylab = 'y_{jkm} - y_{j0m}')
+    #plot(d1, x, main = row.names[i], xlim = c(0, 71), ylim = c(0, 73), lwd = 2, cex.axis = 4, cex.names=4, cex.lab=4, cex.main=4, xlab = 'y_{j0m}', ylab = 'y_{jkm}')
+    #points(c(0, 73), c(0, 73), type = 'l', col = 'blue', lwd = 2)
+    #points(c(0, 73), c(0, 0), type = 'l', col = 'green', lwd = 2)
+    plot(d1iii, x - d1, main = row.names[i], #xlim = c(0, 71), ylim = c(-71, 21),
+         lwd = 2, cex.axis = 4, cex.names=4, cex.lab=4, cex.main=4, xlab = 'iii_{j0m}', ylab = 'y_{jkm} - y_{j0m}')
+    points(c(0, 71), c(0, 0), type = 'l', col = 'blue', lwd = 2)
+    points(c(0, 71), c(0, -71), type = 'l', col = 'green', lwd = 2)
+    plot(xiii - d1iii, x - d1,# xlim = c(0, 71), ylim = c(-71, 21),
+         lwd = 2, cex.axis = 4, cex.names=4, cex.lab=4, cex.main=4, xlab = 'iii_{jkm} - iii_{j0m}', ylab = 'y_{jkm} - y_{j0m}')
     points(c(0, 71), c(0, 0), type = 'l', col = 'blue', lwd = 2)
     points(c(0, 71), c(0, -71), type = 'l', col = 'green', lwd = 2)
 }
