e04c66778404c3486c580a7a1b39556dfd533661
Chris Henry <cjh57@cornell.edu>
2023-04-20 19:12:09 -0400

    Large facility, small crews, including floaters and cleaners.

--- panel-plots-2023-04-03.R
+++ panel-plots-2023-04-03.R
@@ -8,20 +8,20 @@ filenames = c(
     # 'sat2wi-clean/facility-shared-vaccinated_TRUE-recovered_TRUE_diffable-Total-Infections-violin_2.2.0.png',
     # 'sat2wi-clean/facility-shared-vaccinated_TRUE-recovered_TRUE_pairwise-differences-Total-Infections-violin_2.2.0.png',
 
-    'sat2wi-clean/large--alternative--facility-shared-vaccinated_TRUE-recovered_TRUE_Symptomatic_2.2.0.png',
-    'sat2wi-clean/large--alternative--facility-shared-vaccinated_TRUE-recovered_TRUE_Total-Symptomatic-Infections-violin_2.2.0.png',
-    'sat2wi-clean/large--alternative--facility-shared-vaccinated_TRUE-recovered_TRUE_pairwise-differences-Total-Symptomatic-Infections-violin_2.2.0.png',
+    'sat2wi-clean/large--counter-alternative--facility-shared-vaccinated_TRUE-recovered_TRUE_Symptomatic_2.2.0.png',
+    'sat2wi-clean/large--counter-alternative--facility-shared-vaccinated_TRUE-recovered_TRUE_Total-Symptomatic-Infections-violin_2.2.0.png',
+    'sat2wi-clean/large--counter-alternative--facility-shared-vaccinated_TRUE-recovered_TRUE_pairwise-differences-Total-Symptomatic-Infections-violin_2.2.0.png',
 
-    'sat2wi-clean/large--alternative--facility-shared-vaccinated_TRUE-recovered_TRUE_low-mode-pairwise-differences-Total-Symptomatic-Infections-violin_2.2.0.png',
-    'sat2wi-clean/large--alternative--facility-shared-vaccinated_TRUE-recovered_TRUE_high-mode-pairwise-differences-Total-Symptomatic-Infections-violin_2.2.0.png',
-    'sat2wi-clean/large--alternative--facility-shared-vaccinated_TRUE-recovered_TRUE_high-mode-pairwise-percent-differences-Total-Symptomatic-Infections-violin_2.2.0.png'
+    'sat2wi-clean/large--counter-alternative--facility-shared-vaccinated_TRUE-recovered_TRUE_low-mode-pairwise-differences-Total-Symptomatic-Infections-violin_2.2.0.png',
+    'sat2wi-clean/large--counter-alternative--facility-shared-vaccinated_TRUE-recovered_TRUE_high-mode-pairwise-differences-Total-Symptomatic-Infections-violin_2.2.0.png',
+    'sat2wi-clean/large--counter-alternative--facility-shared-vaccinated_TRUE-recovered_TRUE_high-mode-pairwise-percent-differences-Total-Symptomatic-Infections-violin_2.2.0.png'
 
     # 'sat2wi-clean/facility-shared-vaccinated_TRUE-recovered_TRUE_v4-Unavailable-production_2.2.0.png',
     # 'sat2wi-clean/facility-shared-vaccinated_TRUE-recovered_TRUE_diffable-Average-Unavailable-production-violin_2.2.0.png',
     # 'sat2wi-clean/facility-shared-vaccinated_TRUE-recovered_TRUE_pairwise-differences-Average-Unavailable-production-violin_2.2.0.png'
 )
 
-png('large--alternative--new-figure-2-2023-04-11.png', width = 3000, height = 2000)
+png('large--counter-alternative--new-figure-2-2023-04-11.png', width = 3000, height = 2000)
 l = NULL
 for(filename in filenames) {
     this_image = readPNG(filename)
@@ -38,14 +38,14 @@ print(plot_grid(plotlist = l, nrow = 2))#3))
 dev.off()
 
 filenames = c(
-    'sat2wi-clean/large--alternative--facility-shared-vaccinated_TRUE-recovered_TRUE_Unavailable-production_2.2.0.png',
-    'sat2wi-clean/large--alternative--facility-shared-vaccinated_TRUE-recovered_TRUE_Average-Unavailable-production-violin_2.2.0.png',
-    'sat2wi-clean/large--alternative--facility-shared-vaccinated_TRUE-recovered_TRUE_pairwise-differences-Average-Unavailable-production-violin_2.2.0.png'
+    'sat2wi-clean/large--counter-alternative--facility-shared-vaccinated_TRUE-recovered_TRUE_Unavailable-production_2.2.0.png',
+    'sat2wi-clean/large--counter-alternative--facility-shared-vaccinated_TRUE-recovered_TRUE_Average-Unavailable-production-violin_2.2.0.png',
+    'sat2wi-clean/large--counter-alternative--facility-shared-vaccinated_TRUE-recovered_TRUE_pairwise-differences-Average-Unavailable-production-violin_2.2.0.png'
 )
 
 
 
-png('large--alternative--new-figure-3a-2023-04-11.png', width = 3000, height = 1000)
+png('large--counter-alternative--new-figure-3a-2023-04-11.png', width = 3000, height = 1000)
 l = NULL
 for(filename in filenames) {
     this_image = readPNG(filename)
@@ -62,19 +62,19 @@ print(plot_grid(plotlist = l, nrow = 1))#3))
 dev.off()
 
 
-#alternatively
+#counter-alternatively
 #library(png)
 #library(ggplot2)
 #library(grid)
 #library(cowplot)
 
 filenames = c(
-    'sat2wi-clean/large--alternative--facility-shared-vaccinated_TRUE-recovered_TRUE_Total-Intervention-Expenses-violin_2.2.0.png',
-    'sat2wi-clean/large--alternative--facility-shared-vaccinated_TRUE-recovered_TRUE_Total-Production-Loss-violin_2.2.0.png',
-    'sat2wi-clean/large--alternative--facility-shared-vaccinated_TRUE-recovered_TRUE_Total-Cost-violin_2.2.0.png'
+    'sat2wi-clean/large--counter-alternative--facility-shared-vaccinated_TRUE-recovered_TRUE_Total-Intervention-Expenses-violin_2.2.0.png',
+    'sat2wi-clean/large--counter-alternative--facility-shared-vaccinated_TRUE-recovered_TRUE_Total-Production-Loss-violin_2.2.0.png',
+    'sat2wi-clean/large--counter-alternative--facility-shared-vaccinated_TRUE-recovered_TRUE_Total-Cost-violin_2.2.0.png'
 )
 
-png('large--alternative--new-figure-3b-2023-04-11.png', width = 3000, height = 1000)
+png('large--counter-alternative--new-figure-3b-2023-04-11.png', width = 3000, height = 1000)
 l = NULL
 for(filename in filenames) {
     this_image = readPNG(filename)
